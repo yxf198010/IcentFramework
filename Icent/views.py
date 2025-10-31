022bb5a522b05f6de638be631547af4c98ae8757
yxf198010 <yangxinfeng1999@163.com>
2025-10-31 23:47:33 +0800

    升级URL,视图，模板，前台可获取模型数据

--- Icent/views.py
+++ Icent/views.py
@@ -1,7 +1,13 @@
 from django.shortcuts import render
-
+from .models import UserGroup
 # Create your views here.
 
 def index(request):
 	"""埃讯特的主页"""
 	return render(request,'Icent/index.html')
+
+def usergroups(request):
+    """显示所有的用户组"""
+    usergroups = UserGroup.objects.order_by('createdate')
+    context= {'usergroups': usergroups}
+    return render(request,'Icent/usergroups.html', context)
